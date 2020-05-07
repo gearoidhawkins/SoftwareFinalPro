0f727b6d3a8d41c3e6afa2bd27e4579d924fb4e6
AaronMcKinley <aaron09912@gmail.com>
2020-05-07 18:39:15 +0100

    finance finished with tests

--- src/test/java/ie/gmit/FinanceTest.java
+++ src/test/java/ie/gmit/FinanceTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FinanceTest {
 
     private Finance f1;
     ArrayList<Vehicle> VehicleList = new ArrayList<Vehicle>();
+    ArrayList<Vehicle> NoVehiclesList = new ArrayList<Vehicle>();
 
     @BeforeEach
     void init() {
@@ -31,10 +33,19 @@ public class FinanceTest {
         assertEquals(1859.38, f1.monthlyPayments(VehicleList, 4, 5));
     }
 
-//    @Test
-//    void testConstructorIDFail()
-//    {
-//        Exception e = assertThrows(IllegalArgumentException.class, () -> new testClass("Michael", 12) );
-//        assertEquals("Employee Number should be 5 digits", e.getMessage());
-//    }
+    @Test
+    void testListError() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> f1.calculateTotal(NoVehiclesList) );
+        assertEquals("No vehicles in the list", e.getMessage());
+    }
+    @Test
+    void testInterestError() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> f1.monthlyPayments(VehicleList, 4, 20) );
+        assertEquals("Interest Rate is too high as its >10%", e.getMessage());
+    }
+    @Test
+    void testLoanLength() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> f1.monthlyPayments(VehicleList, 6, 5) );
+        assertEquals("Loan length is too long >5 years", e.getMessage());
+    }
 }

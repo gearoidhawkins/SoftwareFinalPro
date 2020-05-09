efc790926706fa67fa21585c7b3ecd96b9a9832c
Gearoid Hawkins <gearoidhawkins@gmail.com>
2020-05-09 16:24:10 +0100

    video demo

--- src/test/java/ie/gmit/CarTest.java
+++ src/test/java/ie/gmit/CarTest.java
@@ -31,7 +31,7 @@ class CarTest {
     @Test
     void testBodyTypeSuccess() {
         Car c3 = new Car(true, 3, "Estate");
-        assertEquals("Estateqwe", c3.getBodyType());
+        assertEquals("Estate", c3.getBodyType());
     }
 
     @Test
